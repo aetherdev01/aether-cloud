a16649d9532aa85fa7a394758efdf779da103d90
aetherdev01 <aldikhoirudin2@gmail.com>
2026-06-30 04:28:20 +0700

    update

--- app/src/main/java/com/aether/cloud/ui/home/ModuleAdapter.kt
+++ app/src/main/java/com/aether/cloud/ui/home/ModuleAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.aether.cloud.data.model.Module
